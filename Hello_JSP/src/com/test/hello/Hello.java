22cb32b27026100ba70084f11951a65a174e7d00
Lim BH <vallo_follow@naver.com>
2016-11-30 11:50:23 +0900

    modify >> Hello.java: 0123456 by Lim

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -3,5 +3,8 @@ package com.test.hello;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("local Repository create Hello_JSP");
+		// 0123456: hotfix edit by Lim
+		System.out.println("hotfix edit/modify");
+		// 0123456: end
 	}
 }
