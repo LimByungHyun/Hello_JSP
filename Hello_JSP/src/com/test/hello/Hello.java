9d567a515d4b0999758f575919083a1ddcd327c7
Lim BH <vallo_follow@naver.com>
2016-12-01 09:23:43 +0900

    modify >> Hello.java:0000001 hotfix by Lim

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -6,5 +6,10 @@ public class Hello {
 		// 0123456: hotfix edit by Lim
 		System.out.println("hotfix edit/modify");
 		// 0123456: end
+		
+		// 0000001: hofix edit by Lim
+		System.out.println("오늘도 웃자 병현아");
+		// 0000001: end
+		
 	}
 }
